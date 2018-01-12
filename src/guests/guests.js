3e0f6404136e1c75b6a586cf56da5912812af265
akgjohansson <johansson.mcquack@gmail.com>
2018-01-12 22:11:04 +0100

    gästlista uppdaterad

--- src/guests/guests.js
+++ src/guests/guests.js
@@ -6,6 +6,9 @@ export class Guests {
   constructor(session) {
     this.session = session;
     this.guestList = null;
+    this.viewGuestList = true;
+    this.viewFoodList = false;
+    this.foodPreferenceList = null;
   }
 
   activate() {
@@ -14,6 +17,39 @@ export class Guests {
         return response.json();
       }).then(data => {
         this.guestList = data;
+        this.generateFoodPreferenceList();
       });
+  }
+
+  viewFoodPreferences() {
+    this.viewGuestList = false;
+    this.viewFoodList = true;
+  }
+
+  viewGuests() {
+    this.viewGuestList = true;
+    this.viewFoodList = false;
+  }
+
+  generateFoodPreferenceList() {
+    let list = [];
+    for (let company of this.guestList) {
+      for (let person of company.Persons) {
+        for (let pref of person.FoodPreferences) {
+          let added = false;
+          for (let i = 0; i < list.length; i++) {
+            if (list[i].name === pref.SwedishName) {
+              list[i].number++;
+              added = true;
+              break;
+            }
+          }
+          if (!added) {
+            list.push({'number': 1, 'name': pref.SwedishName});
+          }
+        }
+      }
+    }
+    this.foodPreferenceList = list;
   }
 }
